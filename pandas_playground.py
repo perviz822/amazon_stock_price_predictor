264f25c03964f781d30a72385638e7d27378fc1b
Parviz <perviz822@gmail.com>
2024-05-10 20:32:06 +0200

    arima for amazon stock prediction  plotting bugs  are fixed, parametres were tuned for better prediction

--- pandas_playground.py
+++ pandas_playground.py
@@ -4,4 +4,4 @@ l2  = [1,23,45,56,23,2,2]
 #usage of data_range
 date_range = pd.date_range(start ='2022-12-01', end ='2022-12-07', freq='D')
 df =  pd.DataFrame(data = {'Sales':l ,'Values':l2}, index=date_range)
-print(df)
+print(type(df.index))
